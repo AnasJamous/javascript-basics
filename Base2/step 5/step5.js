bd001be8aa8f2441f496fe9d673bdf78e8edcaa0
AnasJamous <anas.jam89@gmail.com>
2018-02-20 17:11:01 +0200

    Base2_step5

---
+++ Base2/step 5/step5.js
@@ -0,0 +1,6 @@
+for(let i = 1; i < 6; i++){
+    let img = document.querySelector("#image"+i);
+    img.addEventListener("mouseover", function(){
+        img.src = "images/image"+ i + "_2.jpg"
+    })
+}
